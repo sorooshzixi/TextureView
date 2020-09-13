b7b3aeca3f27e5194332f8b6c6eb87c76a8a3cc5
soroush far <sorooshzix@gmail.com>
2020-09-13 12:40:22 -0500

    Added Car Class

---
+++ CarClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//Car newCar = new Car(textBox1.Text, textBox2.Text, Int32.Parse(textBox3.Text));
+//textBox4.Text = newCar.Age().ToString();
+
+    public class Car
+{
+    int age;
+    public Car(String make, String model, int year)
+    {
+        this.Make = make;
+        this.Model = model;
+        this.Year = year;
+
+    }
+    public String Make
+    { get; set; }
+    public String Model
+    { get; set; }
+    public int Year
+    { get; set; }
+    public int Age()
+    {
+        age = System.DateTime.Today.Year - Year;
+        return (age);
+    }
+}
+
