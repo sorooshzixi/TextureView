c2838da20efe9687251197b1ed9e39df283632b6
soroush far <sorooshzix@gmail.com>
2020-09-13 13:25:55 -0500

    Fixed Directory Layout

--- CarClass.cs
+++ CarClass.cs
@@ -1,28 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 //Car newCar = new Car(textBox1.Text, textBox2.Text, Int32.Parse(textBox3.Text));
 //textBox4.Text = newCar.Age().ToString();
-
-    public class Car
+namespace TextureView
 {
-    int age;
-    public Car(String make, String model, int year)
+    public class Car
     {
-        this.Make = make;
-        this.Model = model;
-        this.Year = year;
+        int age;
+        public Car(String make, String model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
 
-    }
-    public String Make
-    { get; set; }
-    public String Model
-    { get; set; }
-    public int Year
-    { get; set; }
-    public int Age()
-    {
-        age = System.DateTime.Today.Year - Year;
-        return (age);
+        }
+        public String Make
+        { get; set; }
+        public String Model
+        { get; set; }
+        public int Year
+        { get; set; }
+        public int Age()
+        {
+            age = System.DateTime.Today.Year - Year;
+            return (age);
+        }
     }
 }
 
